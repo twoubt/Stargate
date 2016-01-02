87993edbdcf3c0c28fffb48874b34b42d2333240
Schwebel Damien <damien.schw@gmail.com>
2016-01-02 15:08:17 +0100

    MAJ Page Accueil

--- vues/index/accueilpage.php
+++ vues/index/accueilpage.php
@@ -13,13 +13,13 @@
     <h2 class="text-center">Découvrez toutes les fonctionnalités de ce jeu innovant ;)</h2>
     <div class="presentationElement">
       <div id="menuPresentation" class="panel-group">
-          <div class="panel panel-default">
+          <div class="panel panel-default row">
             <div class="panel-heading">
               <a data-toggle="collapse" href="#infoPlanete" data-parent="#menuPresentation">Développez votre planète</a>
             </div>
             <div id="infoPlanete" class="panel-collapse collapse">
-              <img src="" class="img-rounded" alt="une planète">
-              <ul class="list-group">
+              <img src="img/planete_accueil.jpg" class="img-rounded img-center" alt="une planète">
+              <ul class="col-md-12">
                 <li class="list-group-item"><p>Améliorez vos mines pour produire plus de ressources.</p></li>
                 <li class="list-group-item"><p>Construisez des usines et des laboratoires pour améliorer votre technologie.</p></li>
                 <li class="list-group-item"><p>Construisez des vaisseaux pour renforcer votre armada.</p></li>
@@ -28,27 +28,27 @@
               </ul>
             </div>
           </div>
-          <div class="panel panel-default">
+          <div class="panel panel-default row">
             <div class="panel-heading">
               <a data-toggle="collapse" href="#infoVaisseau" data-parent="#menuPresentation">Créez votre vaisseau personnalisé</a>
             </div>
             <div id="infoVaisseau" class="panel-collapse collapse">
-              <img src="" class="img-rounded" alt="Un vaisseau trop swag">
-              <ul class="list-group">
+              <img src="img/vaisseau_accueil.jpg" class="img-rounded img-center" alt="Un vaisseau trop swag">
+              <ul class="col-md-12">
                 <li class="list-group-item"><p>Envoyez votre vaisseau mère dans des aventures pour obtenir toutes sortes de butins.</p></li>
                 <li class="list-group-item"><p>À chaque niveau améliorez ses caractéristiques pour qu'il devienne encore plus puissant.</p></li>
                 <li class="list-group-item"><p>Attaquez d'autre joueur avec votre vaisseau mère pour décupler la puissance de votre flotte.</p></li>
               </ul>
             </div>
           </div>
-          <div class="panel panel-default">
+          <div class="panel panel-default row">
             <div class="panel-heading">
               <a data-toggle="collapse" href="#infoObjectif" data-parent="#menuPresentation">L'objectif qu'est-ce que c'est ?</a>
             </div>
             <div id="infoObjectif" class="panel-collapse collapse">
 
               <ul class="list-group">
-                <li class="list-group-item"><p>Te te développe à mort.</p></li>
+                <li class="list-group-item"><p>Tu te développes à mort.</p></li>
                 <li class="list-group-item"><p>Tu te fait une big armée.</p></li>
                 <li class="list-group-item"><p>Tu casse la bouche à tout le monde.</p></li>
                 <li class="list-group-item"><p>Et enfin tu construit le méga batiment avec la technologie ultra pété que t'as choisit.</p></li>
@@ -59,7 +59,7 @@
     </div>
     </div>
 <!-- div inscription -->
-  <div class="row">
+  <div class="row m-bottom-15">
     <div class="col-md-6 col-md-offset-3">
       <a href="#inscription" data-toggle="collapse">Inscription<a>
       <div id="inscription">
@@ -72,7 +72,7 @@
                 echo "<option value='".($i+1)."'>".$donnees['galaxies'][$i]."</option>";
               } ?>
             </select>
-            <button type="button" id="btnLogin" class="btn">Inscription</button>
+            <button type="button" id="btnLogin" class="btn col-md-3 col-md-offset-9">Inscription</button>
         </form>
       </div>
     </div>
