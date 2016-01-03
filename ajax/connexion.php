dc25540ddb147da79ef8d819316eb3432abc6f72
Schwebel Damien <damien.schw@gmail.com>
2016-01-03 01:26:18 +0100

    Chargement des techno lors de la connexion

--- ajax/connexion.php
+++ ajax/connexion.php
@@ -2,17 +2,22 @@
 require_once "./modele/joueur_modele.php";
 require_once "./modele/planete_modele.php";
 require_once "./modele/batiment_modele.php";
+require_once "./modele/technologie_modele.php";
 require_once "./lib/joueur.php";
 require_once "./lib/batiment.php";
 require_once "./lib/planete.php";
+require_once "./lib/technologie.php";
 $connexion = new Joueur_Modele();
 $valider = $connexion->testConnexion($_POST["username"],$_POST["password"]);
+
 if($valider){
   $planetes = new Planete_Modele();
   $batiment = new Batiment_Modele();
+  $technologies = new Technologie_Modele();
   //On va charger la session avec les informations de l'utilisateur
   $_SESSION["joueur"] = $connexion->loadJoueur($_POST["username"]);
   $id = $_SESSION["joueur"]->getId();
+  //On charge les planetes du joueur
   $_SESSION["planete"] = $planetes->loadAllPlanetes($id);
   $_SESSION["planete_actuel"] = 0; //Indice sur laquelle le joueur va arriver lors de la connexion
   //On charge les batiments dans les planetes
@@ -20,6 +25,8 @@ if($valider){
     $idPlanete = $_SESSION["planete"][$i]->getId();
     $_SESSION["planete"][$i]->setBatiments($batiment->loadBatiments($idPlanete));
   }
+  //On charge les technologies
+  $_SESSION["joueur"]->setTechnologies($technologies->loadTechnologies($id));
   //On l'ajoute dans les joueurs connectés
   $connexion->connexionAJour($id);
   $planetes->calculRessourcesPlanetes($id);
